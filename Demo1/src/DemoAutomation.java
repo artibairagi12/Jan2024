a46666d71742c38c22988c71bbc6ffe54cfd95f6
artib <artib@DESKTOP-P2LGNO3>
2024-03-04 23:01:53 +0530

    Sharing the code first time

---
+++ Demo1/src/DemoAutomation.java
@@ -0,0 +1,40 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import time;
+public class DemoAutomation {
+
+	public static void main(String[] args) {
+		
+		System.getProperty("webdriver.edge.driver", "C:\\C:\\BrowserDrivers//chromedriver.exe");
+		
+		ChromeDriver driver = new ChromeDriver();
+		//EdgeDriver driver = new EdgeDriver();
+		
+		driver.get("http://www.google.com");
+	    //search_box = driver.find_element_by_name("q");
+		//search_box.send_keys("professional tutotrial");
+		//driver.implicitly_wait(5);
+		//search_box.submit();
+		//driver.implicitly_wait(5);
+		//target = driver.find_element_by_xpath(//*[@id="rso"]/div[1]/div/div/div/div/div/div/div/div[1]/a/h3);
+		//print(target.text);
+		//target.click();
+		//time sleep(5);
+		//driver.close();
+		
+		
+		//System.out.println("Hello World");
+		
+		//System.out.print("The sum of these numbers is ");
+		
+		//int A = 6;
+		//int B = 3;
+		//int C = 4;
+		//int sum = A+B+C;
+		//System.out.print("The sum of these numbers is ");
+		//System.out.println(sum);
+		
+			
+		
+	}
+
+}
